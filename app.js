45b1fe0533b2a8128f981203fc2f53fd5f75e067
Eko Yunianto <eyeyunianto@gmail.com>
2014-06-22 10:55:16 +0700

    Edit router.js

--- app.js
+++ app.js
@@ -5,3 +5,6 @@ require('sails').lift(require('optimist').argv);
 // curl -XPOST -d 'client_id=W95ZYDDK3S&client_secret=AVnn5AVPEECHbp84FDStGuvleuuQ0X&grant_type=authorization_code&redirect_uri=http://167.88.34.42:1337&code=CODE' http://167.88.34.42:1337/oauth/token
 
 //curl -XPOST "http://167.88.34.42:1337/oauth/token" -d "grant_type=password&client_id=W95ZYDDK3S&client_secret=AVnn5AVPEECHbp84FDStGuvleuuQ0X&username=me@gmail.com&password=password"
+
+//lsof -i :1337
+//kill -9 2791
